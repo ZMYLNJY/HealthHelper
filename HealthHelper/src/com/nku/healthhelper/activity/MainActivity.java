d9c4702745288f5dce2248412a1fd2e21ee5084c
Eumes-J <eumescan@qq.com>
2017-04-15 01:30:14 +0800

    补充一下

--- HealthHelper/src/com/nku/healthhelper/activity/MainActivity.java
+++ HealthHelper/src/com/nku/healthhelper/activity/MainActivity.java
@@ -9,7 +9,9 @@ public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
+
 }
