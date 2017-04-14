a68bbf42409063f8d8c8cb46876dcc183a245b88
Eumes-J <eumescan@qq.com>
2017-04-14 23:50:28 +0800

    first commit.

---
+++ HealthHelper/src/com/nku/healthhelper/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.nku.healthhelper.activity;
+
+import com.nku.healthhelper.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
